f0989f878f05f48d373a5e6c43ba20779271233f
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 10:58:54 +1300

    03_adder.rb

--- holes/03_adder.rb
+++ holes/03_adder.rb
@@ -3,19 +3,6 @@
 # Each output line should be the corresponding sum
 #
 
-@totals = []
-
-while line = $stdin.gets do
-  line_total = 0
-  numbers = line.scan(/\d+/)
-  numbers = numbers.map { |number| number.to_i }
-  numbers.each do |number|
-    line_total += number.to_i
-  end
-
-  @totals << line_total
-end
-
-@totals.each do |total|
-  $stdout.puts "#{total.to_s}\n"
+while l=gets do
+puts l.scan(/\d+/).map(&:to_i).inject(:+)
 end

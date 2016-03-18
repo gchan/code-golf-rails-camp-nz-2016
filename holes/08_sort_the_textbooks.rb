bf9e64209fda4e66bcb6ad48138149116d611cd4
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 12:31:56 +1300

    08_sort_the_textbooks.rb

--- holes/08_sort_the_textbooks.rb
+++ holes/08_sort_the_textbooks.rb
@@ -35,70 +35,26 @@
 # The output will need to be the same set of books organized in alphabetical order.
 # The height of the books must stay the same and the title must have the same spacing from the top when re-arranged.
 
-class Bookshelf
+b=STDIN.read
 
-  def initialize(input)
-    @input = input
-  end
+b = b.split("\n").map(&:chars).transpose.select.with_index{|l,i| (i+1)%2==0}.map(&:join).sort_by{|a| a.scan(/[^\s_]+/) }
 
-  def sort_alphabetically
-    reordered_books.join "\n"
-  end
+def z(l)i=i(l)
+" "*(i+1)+"|"*(l.size-i-1)end
 
-private
+def i(l)
+l.index("_")end
 
-  def reordered_books
-    new_books = books.dup
-    sorted_titles.each_with_index do |title, book_index|
-      write_book title, book_index, new_books
-    end
-    new_books
-  end
+o=[]
+o<<z(b[0])
 
-  def books
-    @books ||= @input.gsub('|', ' ').split "\n"
-  end
-
-  def transposed_books
-    @transposed_books ||= books.map(&:chars).transpose.map { |chars| chars.join }
-  end
-
-  def sorted_titles
-    @sorted_titles ||= titles.sort_by { |s| s.delete ' _' }
-  end
-
-  def titles
-    @titles ||= transposed_books.each_with_index.select { |line, index| index.odd? }.map { |line, index| line }
-  end
-
-  def write_book(title, book_index, bookshelf)
-    x_position = book_index * 2 + 1
-    on_spine = false
-    letters = title.chars
-
-    # go through each character of each title, including leading spaces and '_'s
-    # this will "descend" down the array writing each letter of the title
-    # along with the "edges"
-
-    letters.each_with_index do |letter, y_position|
-      bookshelf_line = bookshelf[y_position]
-      # If we're on the spine, draw the edges of the book
-      if on_spine == true
-        bookshelf_line[x_position-1] = '|'
-        bookshelf_line[x_position+1] = '|'
-      end
-
-      bookshelf_line[x_position] = letter; # write the current title char into the second (x+1) column
-
-      if ! on_spine && letter == '_'
-        on_spine = true
-      elsif on_spine && letter == '_'
-        on_spine = false
-      end
-    end
-  end
-
+b.each_cons(2) do |a, b|
+o<<a
+m=[i(a),i(b)].min
+o<<" "*(m+1)+"|"*(b.size-m-1)
 end
 
-bookshelf = Bookshelf.new STDIN.read
-puts bookshelf.sort_alphabetically
+o<<b[-1]
+o<<z(b[-1])
+
+puts o.map(&:chars).transpose.map(&:join)

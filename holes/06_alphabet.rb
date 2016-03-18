a2774e76a897ec8fada1f0a79f252e24371bf52a
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 11:11:51 +1300

    06_alphabet.rb

--- holes/06_alphabet.rb
+++ holes/06_alphabet.rb
@@ -1,32 +1,3 @@
 # Print the alphabet. No, not that one.
 
-ALPHABET = [
-  "α",
-  "β",
-  "γ",
-  "δ",
-  "ε",
-  "ζ",
-  "η",
-  "θ",
-  "ι",
-  "κ",
-  "λ",
-  "μ",
-  "ν",
-  "ξ",
-  "ο",
-  "π",
-  "ρ",
-  "σ",
-  "τ",
-  "υ",
-  "φ",
-  "χ",
-  "ψ",
-  "ω",
-]
-
-ALPHABET.each do |letter|
-  printf letter
-end
+print"αβγδεζηθικλμνξοπρστυφχψω"

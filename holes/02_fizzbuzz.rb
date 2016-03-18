6e8202b2dca2d9423361d46b0f3574c7a0a24a21
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 10:58:42 +1300

    02_fizzbuzz.rb

--- holes/02_fizzbuzz.rb
+++ holes/02_fizzbuzz.rb
@@ -5,29 +5,6 @@
 # For multiples of both print both
 # Otherwise print the original number
 
-class Numeric
-  def multiple_of_5?
-    self % 5 == 0
-  end
-
-  def multiple_of_3?
-    self % 3 == 0
-  end
-
-  def multiple_of_15?
-    multiple_of_5? and multiple_of_3?
-  end
-end
-
-while line = gets do
-  i = line.to_i
-  if i.multiple_of_15?
-    puts "FizzBuzz"
-  elsif i.multiple_of_5?
-    puts "Buzz"
-  elsif i.multiple_of_3?
-    puts "Fizz"
-  else
-    puts "#{i.to_s}"
-  end
+while l=gets do i=l.to_i
+puts i%15==0?"FizzBuzz":i%5==0?"Buzz":i%3==0?"Fizz":i
 end

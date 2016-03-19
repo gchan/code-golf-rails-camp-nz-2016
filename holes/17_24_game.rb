e79fd36d2c4c39f61cf858413d0024a85d5196a6
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 15:57:57 +1300

    17_24_game.rb

--- holes/17_24_game.rb
+++ holes/17_24_game.rb
@@ -1,34 +1,16 @@
-module TwentyFourGame
-  EXPRESSIONS = [
-    '((%dr %s %dr) %s %dr) %s %dr',
-    '(%dr %s (%dr %s %dr)) %s %dr',
-    '(%dr %s %dr) %s (%dr %s %dr)',
-    '%dr %s ((%dr %s %dr) %s %dr)',
-    '%dr %s (%dr %s (%dr %s %dr))',
-  ]
-
-  OPERATORS = [:+, :-, :*, :/].repeated_permutation(3).to_a
-
-  def self.solve(digits)
-    solutions = []
-    perms = digits.permutation.to_a.uniq
-    perms.product(OPERATORS, EXPRESSIONS) do |(a,b,c,d), (op1,op2,op3), expr|
-      # evaluate using rational arithmetic
-      text = expr % [a, op1, b, op2, c, op3, d]
-      value = eval(text)  rescue next                 # catch division by zero
-      solutions << text.delete("r")  if value == 24
-    end
-    solutions.sort
-  end
+E=['((%dr %s %dr) %s %dr) %s %dr','(%dr %s (%dr %s %dr)) %s %dr','(%dr %s %dr) %s (%dr %s %dr)','%dr %s ((%dr %s %dr) %s %dr)','%dr %s (%dr %s (%dr %s %dr))']
+O=%i(+ - * /).repeated_permutation(3).to_a
+def s(d)
+s=[]
+p=d.permutation.to_a.uniq
+p.product(O,E){|(a,b,c,d),(o1,o2,o3),e|
+t=e%[a,o1,b,o2,c,o3,d]
+v=eval(t) rescue next
+s<<t.delete("r") if v==24}
+s.sort
 end
 
-# validate user input
-while lines = gets do
-  digits = lines.split.map {|digit| digit.to_i }
-  solutions = TwentyFourGame.solve(digits)
-  if solutions.empty?
-    puts "-"
-  else
-    puts solutions.sort
-  end
+while l=gets do
+s=s(l.split.map(&:to_i))
+puts s.any?? s:"-"
 end

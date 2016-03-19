f26ecd7e401c23d55e79f602899d36af2904a8b4
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 14:13:53 +1300

    14_t9.rb

--- holes/14_t9.rb
+++ holes/14_t9.rb
@@ -3,44 +3,4 @@
 # Translate an input string like '0800-moar-pizza'
 # into its keypad equivalent: 0800-6627-74992
 
-CHAR_MAP = {
-  'a' => 2,
-  'b' => 2,
-  'c' => 2,
-  'd' => 3,
-  'e' => 3,
-  'f' => 3,
-  'g' => 4,
-  'h' => 4,
-  'i' => 4,
-  'j' => 5,
-  'k' => 5,
-  'l' => 5,
-  'm' => 6,
-  'n' => 6,
-  'o' => 6,
-  'p' => 7,
-  'q' => 7,
-  'r' => 7,
-  's' => 7,
-  't' => 8,
-  'u' => 8,
-  'v' => 8,
-  'w' => 9,
-  'x' => 9,
-  'y' => 9,
-  'z' => 9,
-}
-
-STDIN.read.each_line do |line|
-  new_line = ''
-  line.each_char do |char|
-    if CHAR_MAP.has_key? char
-      new_char = CHAR_MAP[char]
-    else
-      new_char = char
-    end
-    new_line << new_char.to_s
-  end
-  puts new_line
-end
+STDIN.read.each_line{|l|puts l.tr("abcdefghijklmnopqrstuvwxyz", "22233344455566677778889999")}

e8a48dee8043018eb9df2c254baa3e8841cc8c6c
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 13:36:49 +1300

    11_grades.rb

--- holes/11_grades.rb
+++ holes/11_grades.rb
@@ -1,23 +1,4 @@
 # Translate percentages to alphabetical grades
 
-STDIN.read.each_line do |line|
-  score = line.to_f
-
-  case score
-  when 90.00..100.00 then
-    puts 'A'
-  when 80.00..89.99 then
-    puts 'B'
-  when 70.00..79.99 then
-    puts 'C'
-  when 60.00..69.99 then
-    puts 'D'
-  when 50.00..59.99 then
-    puts 'E'
-  when  0.00..49.99 then
-    puts 'F'
-  else
-    puts '?'
-  end
-
-end
+STDIN.read.each_line{|l|s=l.to_f
+puts s>90?'A':s>80?'B':s>70?'C':s>60?'D':s>50?'E':s>=0?'F':'?'}

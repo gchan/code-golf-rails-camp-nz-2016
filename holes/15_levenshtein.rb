a8157ecb9c63840aad23ccdadb1428a962e7ec96
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-20 10:20:56 +1300

    Improve 15

--- holes/15_levenshtein.rb
+++ holes/15_levenshtein.rb
@@ -1,9 +1,7 @@
-def d(s, t)m=s.size+1
-n=t.size+1
-d=Array.new(m){Array.new(n,0)}
-(1..m).each{|i|d[i][0]=i}
-(1..n).each{|j|d[0][j]=j}
-(1..n).each{|j|(1..m).each{|i|d[i][j]=[d[i-1][j]+1,d[i][j-1]+1,d[i-1][j-1]+(s[i-1]==t[j-1]?0:1)].min}}
-d[m-1][n-1]end
+def d(s,t)m=s.size
+n=t.size
+d=Array.new(m+1){Array.new(n+1,0)}
+(1..n).each{|j|d[0][j]=j;(1..m).each{|i|d[i][0]=i;d[i][j]=[d[i-1][j]+1,d[i][j-1]+1,d[i-1][j-1]+(s[i-1]==t[j-1]?0:1)].min}}
+d[m][n]end
 
-puts $<.map{|l|a,b=l.split;d(a,b)}
+puts$<.map{|l|d(*l.split)}

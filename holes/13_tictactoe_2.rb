da8cc948c4c1bcddcad20308bd05af2a0230934d
Gordon Chan <developer.gordon+github@gmail.com>
2016-03-19 13:55:22 +1300

    13_tictactoe_2.rb

--- holes/13_tictactoe_2.rb
+++ holes/13_tictactoe_2.rb
@@ -6,63 +6,76 @@
 # The input has an identical format to the ticatactoe_1 hole
 #
 
-class Board
-  InvalidPlacementError = Class.new StandardError
+# class Board
+#   InvalidPlacementError = Class.new StandardError
 
-  # Takes a simple string like e.g.
-  #   "xoxoxooo."
-  #   "x..o...x."
-  #  Each position will correspond to a number from the grid
-  #     1 | 2 | 3
-  #     --+---+--
-  #     4 | 5 | 6
-  #     --+---+--
-  #     7 | 8 | 9
-  def load(input)
-    @positions = input
-    raise InvalidPlacementError if @positions.size > 9
-  end
+#   # Takes a simple string like e.g.
+#   #   "xoxoxooo."
+#   #   "x..o...x."
+#   #  Each position will correspond to a number from the grid
+#   #     1 | 2 | 3
+#   #     --+---+--
+#   #     4 | 5 | 6
+#   #     --+---+--
+#   #     7 | 8 | 9
+#   def load(input)
+#     @positions = input
+#     raise InvalidPlacementError if @positions.size > 9
+#   end
 
-  # returns 'x', 'o' or `nil` for a draw or an incomplete board
-  def winner
-    horizontal_winner || vertical_winner || diagonal_winner
-  end
+#   # returns 'x', 'o' or `nil` for a draw or an incomplete board
+#   def winner
+#     horizontal_winner || vertical_winner || diagonal_winner
+#   end
 
-private
+# private
 
-  def diagonal_winner
-    if @positions[0] != "." && @positions[0] == @positions[4] && @positions[0] == @positions[8]
-      @positions[0]
-    elsif @positions[2] != "." && @positions[2] == @positions[4] && @positions[2] == @positions[6]
-      @positions[2]
-    end
-  end
+#   def diagonal_winner
+#     if i[0] != "." && i[0] == i[4] && i[0] == i[8]
+#       i[0]
+#     elsif i[2] != "." && i[2] == i[4] && i[2] == i[6]
+#       i[2]
+#     end
+#   end
 
-  def horizontal_winner
-    if @positions[0] != "." && @positions[0] == @positions[1] && @positions[0] == @positions[2]
-      @positions[0]
-    elsif @positions[3] != "." && @positions[3] == @positions[4] && @positions[3] == @positions[5]
-      @positions[3]
-    elsif @positions[6] != "." && @positions[6] == @positions[7] && @positions[6] == @positions[8]
-      @positions[6]
-    end
-  end
+#   def horizontal_winner
+#     if i[0] != "." && i[0] == i[1] && i[0] == i[2]
+#       i[0]
+#     elsif i[3] != "." && i[3] == i[4] && i[3] == i[5]
+#       i[3]
+#     elsif i[6] != "." && i[6] == i[7] && i[6] == i[8]
+#       i[6]
+#     end
+#   end
 
-  def vertical_winner
-    if @positions[0] != "." && @positions[0] == @positions[3] && @positions[0] == @positions[6]
-      @positions[0]
-    elsif @positions[1] != "." && @positions[1] == @positions[4] && @positions[1] == @positions[7]
-      @positions[1]
-    elsif @positions[2] != "." && @positions[2] == @positions[5] && @positions[2] == @positions[8]
-      @positions[2]
-    end
-  end
+#   def vertical_winner
+#     if i[0] != "." && i[0] == i[3] && i[0] == i[6]
+#       i[0]
+#     elsif i[1] != "." && i[1] == i[4] && i[1] == i[7]
+#       i[1]
+#     elsif i[2] != "." && i[2] == i[5] && i[2] == i[8]
+#       i[2]
+#     end
+#   end
 
-end
+# end
 
 
-while line = gets do
-  board = Board.new
-  board.load line.strip
-  puts board.winner||'?'
+# while line = gets do
+#   board = Board.new
+#   board.load line.strip
+#   puts board.winner||'?'
+# end
+
+while i=gets do
+puts (if i[0]!="."&&i[0]==i[4]&&i[0]==i[8]
+i[0]elsif i[2]!="."&&i[2]==i[4]&&i[2]==i[6]
+i[2]elsif i[0]!="."&&i[0]==i[1]&&i[0]==i[2]
+i[0]elsif i[3]!="."&&i[3]==i[4]&&i[3]==i[5]
+i[3]elsif i[6]!="."&&i[6]==i[7]&&i[6]==i[8]
+i[6]elsif i[0]!="."&&i[0]==i[3]&&i[0]==i[6]
+i[0]elsif i[1]!="."&&i[1]==i[4]&&i[1]==i[7]
+i[1]elsif i[2]!="."&&i[2]==i[5]&&i[2]==i[8]
+i[2]else
+"?"end)
 end
